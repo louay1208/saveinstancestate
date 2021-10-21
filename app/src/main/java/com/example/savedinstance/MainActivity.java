84d2edb907649ce4f6d34298dafa14c744e08d6d
louay1208 <louay1208@gmail.com>
2021-10-21 11:37:27 +0100

    TP2 challenge 2 savedinstance

---
+++ app/src/main/java/com/example/savedinstance/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.savedinstance;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int mCount=0;
+    private TextView mShowCount;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.textView3);
+        if (savedInstanceState != null) {
+            mCount = savedInstanceState.getInt("count");
+            mShowCount.setText(String.valueOf(mCount));
+        }
+    }
+
+    public void countUp(View view) {
+        ++mCount;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
+
+
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putInt("count", mCount);
+    }
+}
